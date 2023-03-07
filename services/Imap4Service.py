3d9d4d30b0b1ff1d9e42baa6cb9c7be82f3be5d8
Carlos Corrales <ccdevcreative@outlook.com>
2023-03-07 11:38:56 -0500

    adding validation for missing body emails types

--- services/Imap4Service.py
+++ services/Imap4Service.py
@@ -5,6 +5,8 @@ import os
 #from services import *
 #from services import ReferenceEmail
 import yaml
+from datetime import datetime
+from dateutil import parser
 
 from ReferenceEmail import ReferenceEmail
 
@@ -15,6 +17,20 @@ class Imap4Service():
         self.emails = []                
         self.email_list = []
         self.mail_server = None
+        self.username = ""
+        self.password = ""
+        self.file_path = ""
+        self.file_name = ""
+        self.load_configurations()
+
+    def load_configurations(self):
+        with open("settings.yml","r") as f:
+            config = yaml.safe_load(f)
+        
+        self.username = config['login']['username']
+        self.password = config['login']['password']
+        self.file_path = config['generatefile']['filepath']
+        self.file_name = config['generatefile']['filename']
 
     def retrieve_email_reference(self, groupBy = 1):    
         self.connect_to_server()
@@ -37,15 +53,17 @@ class Imap4Service():
 
     def connect_to_server(self,retrieve_from_folder="Filtering",search_by='SUBJECT "Filter"'):
         self.mail_server = imaplib.IMAP4_SSL('outlook.office365.com')        
-        with open("settings.yml", "r") as f:
-            config = yaml.safe_load(f)
-        self.mail_server.login(config['login']['username'], config['login']['password'])
+        # with open("settings.yml", "r") as f:
+        #     config = yaml.safe_load(f)
+        # self.mail_server.login(config['login']['username'], config['login']['password'])
+        self.mail_server.login(self.username, self.password)
         self.mail_server.select(retrieve_from_folder) # can be "INBOX" also
         typ, folders = self.mail_server.list()
         status, emails = self.mail_server.search(None, search_by) # can be ALL also        
         self.email_ids = emails[0].split()
         
-                 
+   
+
     
     # def retrieve_email_reference(self):
         
@@ -72,22 +90,25 @@ class Imap4Service():
     def retrieve_email_reference_groupby_none(self):        
         for email_id in self.email_ids:
             status, msg = self.mail_server.fetch(email_id,'(RFC822)')
-            if status == 'OK':                
-                msg = email.message_from_bytes(msg[0][1])                    
+            if status == 'OK':
+                #msg = email.message_from_bytes(msg[0][1])                    
                 try:
+                    if(isinstance(email.message_from_bytes(msg[0][1])._payload[0],email.message.Message)):
+                        body_url = email.message_from_bytes(msg[0][1])._payload[0]._payload.replace(os.linesep,'').rstrip("Get Outlook for iOS<https://aka.ms/o0ukef>")
+                        dateformated  = parser.parse(email.message_from_bytes(msg[0][1])["Date"]).strftime('%c')
+                    else:
+                        body_url = ''
                     # body = msg._payload[0]._payload.replace(os.linesep,"").rstrip(os.linesep+"Get Outlook for iOS<https://aka.ms/o0ukef>"+os.linesep+)
-                    body = msg._payload[0]._payload.rstrip(os.linesep+"Get Outlook for iOS<https://aka.ms/o0ukef>"+os.linesep)
-                except:
-                    body = f'Not able to get email payload in {email_id} with details,subject: {msg["Subject"]} , Date: {msg["Date"]}'
-                    pass
-                self.emails.append(ReferenceEmail(msg["Subject"],msg["Date"],body))
-                
-                #TODO: log this error instead
-                print(f'Subject: {msg["Subject"]} , Date: {msg["Date"]} ')
-                print(f'From: {msg["From"]}, ')                                
-                print(f'Body: {body}')
-               
-                
+                    
+                    self.emails.append(ReferenceEmail(email.message_from_bytes(msg[0][1])["Subject"],dateformated,body_url))
+
+                    #TODO: log this error instead
+                    print(f'Subject: {email.message_from_bytes(msg[0][1])["Subject"]} , Date: {dateformated}')
+                    print(f'From: {email.message_from_bytes(msg[0][1])["From"]}, ')                                
+                    print(f'Body: {body_url}')
+                except ValueError as ve1:
+                    print('ValueError 1:', ve1)
+                    body = f'Not able to get email payload in {email_id} with details,subject: {email.message_from_bytes(msg[0][1])["Subject"]} , Date: {dateformated}'                    
     
     def retrieve_email_reference_groupby_date(self):
         pass        
@@ -96,14 +117,22 @@ class Imap4Service():
     def retrieve_email_reference_groupby_tag_and_date(self):
         pass    
     
-    def write_md_file(self, file_path="C:/", file_name="filtered.md"):        
+    def write_md_file(self):        
         count = 1
-        with open(os.path.join(file_path,file_name),"a") as file:
-            for email_item in self.emails:                
-                row = f'{count}. [ ] {email_item.content}, {email_item.date}'            
-                row += os.linesep
-                file.write(row)
-                count+=1   
+        with open(os.path.join(self.file_path,self.file_name),"w") as file:
+            for email_item in self.emails:      
+                try: 
+                    if (email_item.url != ''):
+                        row = f'{count}. [ ] {email_item.url}, {email_item.date}'            
+                        row += os.linesep
+                        file.write(row)
+                        count+=1   
+                except:
+                    print(f"error accessing email data: {count}")
+        
+        #print(f"file {str.join(self.file_name,self.file_name)} ")
+
+                
 
 class GroupBy(Enum):
     ByNone = 1    
